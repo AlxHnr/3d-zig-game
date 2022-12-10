fe574165df93a8d38f712a9dccb363608cfbd191
Alexander Heinrich <alxhnr@users.noreply.github.com>
2022-12-10 20:40:43 +0100

    Draw cube

--- src/main.zig
+++ src/main.zig
@@ -18,10 +18,22 @@ pub fn main() !void {
     rl.InitWindow(screenWidth, screenHeight, "3D Zig Game");
     defer rl.CloseWindow();
 
+    var camera = std.mem.zeroes(rl.Camera);
+    camera.position = rl.Vector3{ .x = 5.0, .y = 5.0, .z = 5.0 };
+    camera.target = rl.Vector3{ .x = 0.0, .y = 1.0, .z = 0.0 };
+    camera.up = rl.Vector3{ .x = 0.0, .y = 1.0, .z = 0.0 };
+    camera.fovy = 45.0;
+    camera.projection = rl.CameraProjection.CAMERA_PERSPECTIVE;
+    rl.SetCameraMode(camera, rl.CameraMode.CAMERA_THIRD_PERSON);
+
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
-
         rl.ClearBackground(rl.WHITE);
+
+        rl.BeginMode3D(camera);
+        rl.DrawCube(rl.Vector3{ .x = 0.0, .y = 1.0, .z = 0.0 }, 2.0, 2.0, 2.0, rl.BLUE);
+        rl.DrawGrid(20, 1.0);
+        rl.EndMode3D();
 
         drawFpsCounter();
         rl.EndDrawing();
