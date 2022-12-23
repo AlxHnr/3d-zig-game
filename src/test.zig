5ca915ac89ccdaf7c8b8530b5c312072e35985f5
Alexander Heinrich <alxhnr@users.noreply.github.com>
2022-12-23 14:17:38 +0100

    Fix tests

--- src/test.zig
+++ src/test.zig
@@ -13,10 +13,10 @@ test "Create collision rectangle" {
         19.18,
     );
     const expected_angle = util.degreesToRadians(10.653624);
-    try expectApproxEqRel(@floatCast(f32, 11.220045), rectangle.bottom_left_corner.x, epsilon);
-    try expectApproxEqRel(@floatCast(f32, 26.3245), rectangle.bottom_left_corner.z, epsilon);
-    try expectApproxEqRel(@floatCast(f32, 30.400045), rectangle.top_right_corner.x, epsilon);
-    try expectApproxEqRel(@floatCast(f32, -5.265016), rectangle.top_right_corner.z, epsilon);
+    try expectApproxEqRel(@floatCast(f32, 11.220045), rectangle.first_corner.x, epsilon);
+    try expectApproxEqRel(@floatCast(f32, 26.3245), rectangle.first_corner.z, epsilon);
+    try expectApproxEqRel(@floatCast(f32, 30.400045), rectangle.third_corner.x, epsilon);
+    try expectApproxEqRel(@floatCast(f32, -5.265016), rectangle.third_corner.z, epsilon);
     try expectApproxEqRel(std.math.sin(expected_angle), rectangle.rotation.sine, epsilon);
     try expectApproxEqRel(std.math.cos(expected_angle), rectangle.rotation.cosine, epsilon);
     try expectApproxEqRel(std.math.sin(-expected_angle), rectangle.inverse_rotation.sine, epsilon);
