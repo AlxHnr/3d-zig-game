d8414b1a07aa7c04364a96f7b7c9639f85ba0a03
Alexander Heinrich <alxhnr@users.noreply.github.com>
2023-01-05 03:12:39 +0100

    Load textures into array texture

--- src/textures.zig
+++ src/textures.zig
@@ -5,7 +5,8 @@ const rl = @import("raylib");
 const glad = @cImport(@cInclude("external/glad.h"));
 const Error = @import("error.zig").Error;
 
-pub const Name = enum {
+/// The ordinal values are passed to shaders to index array textures.
+pub const Name = enum(u8) {
     gem,
     grass,
     hedge,
@@ -18,6 +19,80 @@ pub const Name = enum {
     water_frame_1,
     water_frame_2,
 };
+
+/// Returns the id of an OpenGL array texture containing all the textures in the `Name` enum. The
+/// returned id binds to GL_TEXTURE_2D_ARRAY and has to be destroyed via glDeleteTextures(1, &id).
+pub fn loadTextureArray() !c_uint {
+    var id: c_uint = undefined;
+    glad.glGenTextures(1, &id);
+    if (id == 0) {
+        return Error.FailedToLoadTextureFile;
+    }
+    errdefer glad.glDeleteTextures(1, &id);
+
+    glad.glBindTexture(glad.GL_TEXTURE_2D_ARRAY, id);
+    defer glad.glBindTexture(glad.GL_TEXTURE_2D_ARRAY, 0);
+
+    const texture_width = 64; // Smaller and larger images get scaled to this.
+    const texture_height = texture_width;
+    const mipmap_levels = std.math.log2(texture_width);
+    glad.glTexStorage3D(
+        glad.GL_TEXTURE_2D_ARRAY,
+        mipmap_levels,
+        glad.GL_RGBA8,
+        texture_width,
+        texture_height,
+        @typeInfo(Name).Enum.fields.len,
+    );
+    glad.glTexParameteri(glad.GL_TEXTURE_2D_ARRAY, glad.GL_TEXTURE_WRAP_S, glad.GL_REPEAT);
+    glad.glTexParameteri(glad.GL_TEXTURE_2D_ARRAY, glad.GL_TEXTURE_WRAP_T, glad.GL_REPEAT);
+    glad.glTexParameteri(glad.GL_TEXTURE_2D_ARRAY, glad.GL_TEXTURE_MAG_FILTER, glad.GL_NEAREST);
+    glad.glTexParameteri(
+        glad.GL_TEXTURE_2D_ARRAY,
+        glad.GL_TEXTURE_MIN_FILTER,
+        glad.GL_LINEAR_MIPMAP_NEAREST,
+    );
+
+    for (std.enums.values(Name)) |value, index| {
+        var path_buffer: [64]u8 = undefined;
+        const texture_path = try std.fmt.bufPrintZ(path_buffer[0..], "assets/{s}.png", .{
+            @tagName(value),
+        });
+
+        var image = rl.LoadImage(texture_path);
+        if (image.data == null) {
+            return Error.FailedToLoadTextureFile;
+        }
+        defer rl.UnloadImage(image);
+
+        rl.ImageFormat(&image, @enumToInt(rl.PIXELFORMAT_UNCOMPRESSED_R8G8B8A8));
+        if (image.format != rl.PIXELFORMAT_UNCOMPRESSED_R8G8B8A8) {
+            std.log.err("Image has wrong format, expected 8-bit RGBA: \"{s}\"\n", .{texture_path});
+            return Error.FailedToLoadTextureFile;
+        }
+        if (image.width != texture_width or image.height != texture_height) {
+            rl.ImageResizeNN(&image, texture_width, texture_height);
+        }
+
+        glad.glTexSubImage3D(
+            glad.GL_TEXTURE_2D_ARRAY,
+            0,
+            0,
+            0,
+            @intCast(c_int, index),
+            texture_width,
+            texture_height,
+            1,
+            glad.GL_RGBA,
+            glad.GL_UNSIGNED_BYTE,
+            image.data,
+        );
+    }
+
+    glad.glGenerateMipmap(glad.GL_TEXTURE_2D_ARRAY);
+    return id;
+}
+
 pub const Collection = struct {
     textures: std.EnumArray(Name, rl.Texture),
 
