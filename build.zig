c88a415e5b98390f48ec4b4f6dfe644e72f893c2
Alexander Heinrich <alxhnr@users.noreply.github.com>
2023-04-22 22:42:20 +0200

    Fix build with latest zig compiler

--- build.zig
+++ build.zig
@@ -9,9 +9,9 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("SDL2_image");
     exe.addAnonymousModule("gl", .{ .source_file = .{ .path = "third_party/gl.zig" } });
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
