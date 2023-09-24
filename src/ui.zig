045c06b3b827b835f418375a522022c9ebc4afbb
Alexander Heinrich <alxhnr@users.noreply.github.com>
2023-09-24 20:26:04 +0200

    Add boxed text ui element

--- src/ui.zig
+++ src/ui.zig
@@ -116,3 +116,177 @@ pub const ImageWithText = struct {
         };
     }
 };
+
+pub const BoxedText = struct {
+    wrapped_image_with_text: ImageWithText,
+    /// Dimensions. Assumed to be the same for all dialog box sprites.
+    scaled_sprite: struct { width: f32, height: f32 },
+
+    const box_sprite_count = 9;
+    const dialog_sprite_scale = 4;
+
+    pub fn create(
+        sprite_sheet: SpriteSheetTexture,
+        /// 1 means the original character size in pixels.
+        text_scale: u8,
+    ) BoxedText {
+        const dummy_value = .dialog_box_top_left;
+        return createWithImage(dummy_value, 0, sprite_sheet, text_scale);
+    }
+
+    pub fn createWithImage(
+        sprite: SpriteSheetTexture.SpriteId,
+        /// 1 means the original size in pixels.
+        sprite_scale: u16,
+        sprite_sheet: SpriteSheetTexture,
+        /// 1 means the original character size in pixels.
+        text_scale: u8,
+    ) BoxedText {
+        // Assumed to be the same for all dialog box sprites.
+        const dimensions = sprite_sheet.getSpriteDimensionsInPixels(.dialog_box_top_left);
+        return .{
+            .wrapped_image_with_text = ImageWithText.create(
+                sprite,
+                sprite_scale,
+                sprite_sheet,
+                text_scale,
+            ),
+            .scaled_sprite = .{
+                .width = @as(f32, @floatFromInt(dimensions.w)) * dialog_sprite_scale,
+                .height = @as(f32, @floatFromInt(dimensions.h)) * dialog_sprite_scale,
+            },
+        };
+    }
+
+    pub fn getBillboardCount(self: BoxedText, segments: []const text_rendering.TextSegment) usize {
+        return box_sprite_count + self.wrapped_image_with_text.getBillboardCount(segments);
+    }
+
+    pub fn populateBillboardData(
+        self: BoxedText,
+        /// Top left corner.
+        screen_position_x: u16,
+        screen_position_y: u16,
+        sprite_sheet: SpriteSheetTexture,
+        segments: []const text_rendering.TextSegment,
+        /// Must have enough capacity to store all billboards. See getBillboardCount().
+        out: []BillboardData,
+    ) void {
+        const content_dimensions_u16 =
+            self.wrapped_image_with_text.getDimensionsInPixels(segments, sprite_sheet);
+        const content = .{
+            .w = @as(f32, @floatFromInt(content_dimensions_u16.width)),
+            .h = @as(f32, @floatFromInt(content_dimensions_u16.height)),
+        };
+        const sprite = .{ .w = self.scaled_sprite.width, .h = self.scaled_sprite.height };
+
+        const helper = BillboardDataHelper.create(
+            sprite_sheet,
+            screen_position_x,
+            screen_position_y,
+            sprite.w,
+            sprite.h,
+            out,
+        );
+        helper.insert(.dialog_box_top_left, 0, 0, sprite.w, sprite.h);
+        helper.insert(.dialog_box_top_center, sprite.w, 0, content.w, sprite.h);
+        helper.insert(.dialog_box_top_right, sprite.w + content.w, 0, sprite.w, sprite.h);
+        helper.insert(.dialog_box_center_left, 0, sprite.h, sprite.w, content.h);
+        helper.insert(.dialog_box_center_left, 0, sprite.h, sprite.w, content.h);
+        helper.insert(.dialog_box_center_center, sprite.w, sprite.h, content.w, content.h);
+        helper.insert(.dialog_box_center_right, sprite.w + content.w, sprite.h, sprite.w, content.h);
+        helper.insert(.dialog_box_bottom_left, 0, sprite.h + content.h, sprite.w, sprite.h);
+        helper.insert(.dialog_box_bottom_center, sprite.w, sprite.h + content.h, content.w, sprite.h);
+        helper.insert(.dialog_box_bottom_right, sprite.w + content.w, sprite.h + content.h, sprite.w, sprite.h);
+
+        self.wrapped_image_with_text.populateBillboardData(
+            screen_position_x + @as(u16, @intFromFloat(sprite.w)),
+            screen_position_y + @as(u16, @intFromFloat(sprite.h)),
+            sprite_sheet,
+            segments,
+            out[box_sprite_count..],
+        );
+    }
+
+    pub fn getDimensionsInPixels(
+        self: BoxedText,
+        segments: []const text_rendering.TextSegment,
+        sprite_sheet: SpriteSheetTexture,
+    ) ScreenDimensions {
+        const content = self.wrapped_image_with_text.getDimensionsInPixels(segments, sprite_sheet);
+        return .{
+            .width = content.width + @as(u16, @intFromFloat(self.scaled_sprite.width)) * 2,
+            .height = content.height + @as(u16, @intFromFloat(self.scaled_sprite.height)) * 2,
+        };
+    }
+
+    const BillboardDataHelper = struct {
+        sprite_sheet: SpriteSheetTexture,
+        top_left_corner: struct { x: f32, y: f32 },
+        /// Dimensions. Assumed to be the same for all dialog box sprites.
+        scaled_sprite: struct { width: f32, height: f32 },
+        out: []BillboardData,
+
+        fn create(
+            sprite_sheet: SpriteSheetTexture,
+            screen_position_x: u16,
+            screen_position_y: u16,
+            scaled_sprite_width: f32,
+            scaled_sprite_height: f32,
+            out: []BillboardData,
+        ) BillboardDataHelper {
+            return .{
+                .sprite_sheet = sprite_sheet,
+                .top_left_corner = .{
+                    .x = @as(f32, @floatFromInt(screen_position_x)),
+                    .y = @as(f32, @floatFromInt(screen_position_y)),
+                },
+                .scaled_sprite = .{
+                    .width = scaled_sprite_width,
+                    .height = scaled_sprite_height,
+                },
+                .out = out,
+            };
+        }
+
+        fn insert(
+            self: BillboardDataHelper,
+            corner: SpriteSheetTexture.SpriteId,
+            offset_from_top_left_x: f32,
+            offset_from_top_left_y: f32,
+            width: f32,
+            height: f32,
+        ) void {
+            const sprite_texcoords = self.sprite_sheet.getSpriteTexcoords(corner);
+            self.out[getIndex(corner)] = .{
+                .position = .{
+                    .x = self.top_left_corner.x + offset_from_top_left_x + width / 2,
+                    .y = self.top_left_corner.y + offset_from_top_left_y + height / 2,
+                    .z = 0,
+                },
+                .size = .{ .w = width, .h = height },
+                .source_rect = .{
+                    .x = sprite_texcoords.x,
+                    .y = sprite_texcoords.y,
+                    .w = sprite_texcoords.w,
+                    .h = sprite_texcoords.h,
+                },
+            };
+        }
+
+        fn getIndex(corner: SpriteSheetTexture.SpriteId) usize {
+            return switch (corner) {
+                .dialog_box_bottom_center => 0,
+                .dialog_box_bottom_left => 1,
+                .dialog_box_bottom_right => 2,
+                .dialog_box_center_center => 3,
+                .dialog_box_center_left => 4,
+                .dialog_box_center_right => 5,
+                .dialog_box_top_center => 6,
+                .dialog_box_top_left => 7,
+                .dialog_box_top_right => 8,
+                else => unreachable,
+            };
+        }
+    };
+};
