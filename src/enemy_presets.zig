8b784681b70f02bdb2e783feeaf2e7a87fd8b980
Alexander Heinrich <alxhnr@users.noreply.github.com>
2023-10-03 11:50:01 +0200

    Add enemy presets

---
+++ src/enemy_presets.zig
@@ -0,0 +1,10 @@
+const Config = @import("game_unit.zig").Enemy.Configuration;
+
+pub const floating_eye = Config{
+    .name = "Floating Eye",
+    .sprite = .yellow_floating_eye,
+    .height = 2,
+    .movement_speed = 0.1,
+    .max_health = 70,
+    .aggro_radius = 20,
+};
