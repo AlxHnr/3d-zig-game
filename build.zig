fb1d9d9e3210b58f8bda82b4a9f1d3941f526334
Alexander Heinrich <alxhnr@users.noreply.github.com>
2023-10-05 20:25:32 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,9 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "3d-zig-game",
         .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
     });
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("SDL2");
@@ -19,7 +24,11 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the game");
     run_step.dependOn(&run_cmd.step);
 
-    const tests = b.addTest(.{ .root_source_file = .{ .path = "src/test.zig" } });
+    const tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/test.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     const test_run_artifact = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&test_run_artifact.step);
