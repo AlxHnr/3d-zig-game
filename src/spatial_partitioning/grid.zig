cd7435d29a5321d07a470409921d527efaa7bd2a
Alexander Heinrich <alxhnr@users.noreply.github.com>
2023-10-19 21:09:45 +0200

    Rename various members in spatial grid

--- src/spatial_partitioning/grid.zig
+++ src/spatial_partitioning/grid.zig
@@ -10,34 +10,34 @@ const std = @import("std");
 pub fn Grid(comptime T: type, comptime cell_side_length: u32) type {
     return struct {
         allocator: std.mem.Allocator,
-        cells: std.AutoHashMap(CellIndex, Cell),
+        cells: std.AutoHashMap(CellIndex, UnorderedCollection(T)),
         /// Maps object ids to references of all cells containing copies of the object.
-        object_ids_to_cell_items: std.AutoHashMap(u64, CellReferenceList),
+        object_ids_to_cell_references: std.AutoHashMap(u64, CellReferenceList),
         /// Counterpart to previous table.
-        cell_items_to_object_ids: std.AutoHashMap(*const CellItem, u64),
+        object_ptrs_to_object_ids: std.AutoHashMap(*const T, u64),
 
         const Self = @This();
-        const Cell = UnorderedCollection(CellItem);
         const CellIndex = @import("cell_index.zig").Index(cell_side_length);
         const CellRange = @import("cell_range.zig").Range(cell_side_length);
 
         pub fn create(allocator: std.mem.Allocator) Self {
             return .{
                 .allocator = allocator,
-                .cells = std.AutoHashMap(CellIndex, Cell).init(allocator),
-                .object_ids_to_cell_items = std.AutoHashMap(u64, CellReferenceList).init(allocator),
-                .cell_items_to_object_ids = std.AutoHashMap(*const CellItem, u64).init(allocator),
+                .cells = std.AutoHashMap(CellIndex, UnorderedCollection(T)).init(allocator),
+                .object_ids_to_cell_references = std.AutoHashMap(u64, CellReferenceList)
+                    .init(allocator),
+                .object_ptrs_to_object_ids = std.AutoHashMap(*const T, u64).init(allocator),
             };
         }
 
         pub fn destroy(self: *Self) void {
-            self.cell_items_to_object_ids.deinit();
+            self.object_ptrs_to_object_ids.deinit();
 
-            var ref_iterator = self.object_ids_to_cell_items.valueIterator();
+            var ref_iterator = self.object_ids_to_cell_references.valueIterator();
             while (ref_iterator.next()) |cell_references| {
                 self.allocator.free(cell_references.items);
             }
-            self.object_ids_to_cell_items.deinit();
+            self.object_ids_to_cell_references.deinit();
 
             var cell_iterator = self.cells.valueIterator();
             while (cell_iterator.next()) |cell| {
@@ -49,12 +49,12 @@ pub fn Grid(comptime T: type, comptime cell_side_length: u32) type {
         /// Reset this grid, including all of its cells, to an empty state. Preserves its allocated
         /// capacity. Invalidates all existing iterators and pointers to objects in this grid.
         pub fn resetPreservingCapacity(self: *Self) void {
-            self.cell_items_to_object_ids.clearRetainingCapacity();
-            var ref_iterator = self.object_ids_to_cell_items.valueIterator();
+            self.object_ptrs_to_object_ids.clearRetainingCapacity();
+            var ref_iterator = self.object_ids_to_cell_references.valueIterator();
             while (ref_iterator.next()) |cell_references| {
                 self.allocator.free(cell_references.items);
             }
-            self.object_ids_to_cell_items.clearRetainingCapacity();
+            self.object_ids_to_cell_references.clearRetainingCapacity();
             var cell_iterator = self.cells.valueIterator();
             while (cell_iterator.next()) |cell| {
                 cell.resetPreservingCapacity();
@@ -77,8 +77,8 @@ pub fn Grid(comptime T: type, comptime cell_side_length: u32) type {
             };
             errdefer self.allocator.free(cell_reference_list.items);
 
-            try self.object_ids_to_cell_items.putNoClobber(object_id, cell_reference_list);
-            errdefer _ = self.object_ids_to_cell_items.remove(object_id);
+            try self.object_ids_to_cell_references.putNoClobber(object_id, cell_reference_list);
+            errdefer _ = self.object_ids_to_cell_references.remove(object_id);
 
             var cell_counter: usize = 0;
             errdefer self.destroyPartialReferencesDuringInsert(
@@ -86,49 +86,53 @@ pub fn Grid(comptime T: type, comptime cell_side_length: u32) type {
             );
 
             var it = cell_range.iterator();
-            while (it.next()) |index| : (cell_counter += 1) {
-                const cell = try self.cells.getOrPut(index);
+            while (it.next()) |cell_index| : (cell_counter += 1) {
+                const cell = try self.cells.getOrPut(cell_index);
                 if (!cell.found_existing) {
-                    cell.value_ptr.* = Cell.create(self.allocator);
+                    cell.value_ptr.* = UnorderedCollection(T).create(self.allocator);
                 }
                 errdefer if (!cell.found_existing) {
                     cell.value_ptr.destroy();
-                    _ = self.cells.remove(index);
+                    _ = self.cells.remove(cell_index);
                 };
 
-                const item = try cell.value_ptr.appendUninitialized();
+                const object_ptr = try cell.value_ptr.appendUninitialized();
                 errdefer cell.value_ptr.removeLastAppendedItem();
-                item.* = .{ .cell_range = cell_range, .object = object };
+                object_ptr.* = object;
 
-                try self.cell_items_to_object_ids.putNoClobber(item, object_id);
-                errdefer _ = self.cell_items_to_object_ids.remove(item);
+                try self.object_ptrs_to_object_ids.putNoClobber(object_ptr, object_id);
+                errdefer _ = self.object_ptrs_to_object_ids.remove(object_ptr);
 
-                cell_reference_list.items[cell_counter] = .{ .index = index, .item = item };
+                cell_reference_list.items[cell_counter] = .{
+                    .cell_index = cell_index,
+                    .object_ptr = object_ptr,
+                };
             }
         }
 
         /// The specified object id must exist in this grid. Invalidates existing iterators.
         /// Preserves the grids capacity.
         pub fn remove(self: *Self, object_id: u64) void {
-            const key_value_pair = self.object_ids_to_cell_items.fetchRemove(object_id);
+            const key_value_pair = self.object_ids_to_cell_references.fetchRemove(object_id);
             std.debug.assert(key_value_pair != null);
             const cell_references = key_value_pair.?.value;
 
             for (cell_references.items) |cell_reference| {
-                const cell = self.cells.getPtr(cell_reference.index).?;
-                if (cell.swapRemove(cell_reference.item)) |displaced_item_address| {
+                const cell = self.cells.getPtr(cell_reference.cell_index).?;
+                if (cell.swapRemove(cell_reference.object_ptr)) |displaced_item_address| {
                     const displaced_object_id =
-                        self.cell_items_to_object_ids.fetchRemove(displaced_item_address).?.value;
-                    self.cell_items_to_object_ids.getPtr(cell_reference.item).?.* =
+                        self.object_ptrs_to_object_ids.fetchRemove(displaced_item_address).?.value;
+                    self.object_ptrs_to_object_ids.getPtr(cell_reference.object_ptr).?.* =
                         displaced_object_id;
-                    const items = self.object_ids_to_cell_items.get(displaced_object_id).?.items;
+                    const items =
+                        self.object_ids_to_cell_references.get(displaced_object_id).?.items;
                     for (items) |*reference_to_displaced_object| {
-                        if (reference_to_displaced_object.item == displaced_item_address) {
-                            reference_to_displaced_object.item = cell_reference.item;
+                        if (reference_to_displaced_object.object_ptr == displaced_item_address) {
+                            reference_to_displaced_object.object_ptr = cell_reference.object_ptr;
                         }
                     }
                 } else {
-                    _ = self.cell_items_to_object_ids.remove(cell_reference.item);
+                    _ = self.object_ptrs_to_object_ids.remove(cell_reference.object_ptr);
                 }
             }
             self.allocator.free(cell_references.items);
@@ -164,14 +168,7 @@ pub fn Grid(comptime T: type, comptime cell_side_length: u32) type {
             cell_line_iterator.Iterator(CellIndex),
         );
 
-        const CellItem = struct {
-            /// Contains all cells occupied by this item. Used for determining whether this item has
-            /// already been visited by an iterator.
-            cell_range: CellRange,
-            object: T,
-        };
-
-        const CellReference = struct { index: CellIndex, item: *CellItem };
+        const CellReference = struct { cell_index: CellIndex, object_ptr: *T };
         const CellReferenceList = struct { items: []CellReference };
 
         fn destroyPartialReferencesDuringInsert(
@@ -179,22 +176,22 @@ pub fn Grid(comptime T: type, comptime cell_side_length: u32) type {
             cell_references: []CellReference,
         ) void {
             for (cell_references) |cell_reference| {
-                _ = self.cell_items_to_object_ids.remove(cell_reference.item);
+                _ = self.object_ptrs_to_object_ids.remove(cell_reference.object_ptr);
 
-                const cell = self.cells.getPtr(cell_reference.index).?;
+                const cell = self.cells.getPtr(cell_reference.cell_index).?;
                 cell.removeLastAppendedItem();
                 if (cell.count() == 0) {
                     cell.destroy();
-                    _ = self.cells.remove(cell_reference.index);
+                    _ = self.cells.remove(cell_reference.cell_index);
                 }
             }
         }
 
         fn ConstBaseIterator(comptime IndexIterator: type) type {
             return struct {
-                cells: *const std.AutoHashMap(CellIndex, Cell),
+                cells: *const std.AutoHashMap(CellIndex, UnorderedCollection(T)),
                 index_iterator: IndexIterator,
-                cell_iterator: ?Cell.ConstIterator,
+                cell_iterator: ?UnorderedCollection(T).ConstIterator,
 
                 pub fn next(self: *@This()) ?T {
                     if (self.nextFromCellIterator()) |object| {
@@ -213,8 +210,8 @@ pub fn Grid(comptime T: type, comptime cell_side_length: u32) type {
 
                 fn nextFromCellIterator(self: *@This()) ?T {
                     if (self.cell_iterator) |*cell_iterator| {
-                        if (cell_iterator.next()) |item| {
-                            return item.object;
+                        if (cell_iterator.next()) |object| {
+                            return object.*;
                         }
                         self.cell_iterator = null;
                     }
